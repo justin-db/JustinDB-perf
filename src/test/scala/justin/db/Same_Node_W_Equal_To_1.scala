17be5014403a906940a8a6b333b68b758e4a128c
speedcom <mateusz.maciaszekhpc@gmail.com>
2016-12-15 12:38:44 +0100

    first basic gatling scenario: "Send data to the same node with w=1 option"

---
+++ src/test/scala/justin/db/Same_Node_W_Equal_To_1.scala
@@ -0,0 +1,26 @@
+package justin.db
+
+import java.util.UUID
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class Same_Node_W_Equal_To_1 extends Simulation {
+
+  val scn = scenario("Send data to the same node with w=1 option")
+    .exec(http("PUT data")
+      .post("http://localhost:9000/put")
+      .header(HttpHeaderNames.ContentType, HttpHeaderValues.ApplicationJson)
+      .body(StringBody(s"""{
+          "id"    : "${UUID.randomUUID().toString}",
+          "value" : "to jest test",
+          "w"     : 1
+        }"""))
+      .asJSON
+      .check(status.is(204))
+    )
+    .pause(5.seconds)
+
+  setUp(scn.inject(atOnceUsers(1)))
+}
