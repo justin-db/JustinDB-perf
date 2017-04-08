babae09e9d6e37fbcc69a2e8357ff58cd75a0a09
speedcom <mateusz.maciaszekhpc@gmail.com>
2017-04-08 20:39:35 +0200

    send different PUT body with 10 concurrent users and repetition of every call

--- src/test/scala/justin/db/Same_Node_W_Equal_To_3.scala
+++ src/test/scala/justin/db/Same_Node_W_Equal_To_3.scala
@@ -9,19 +9,22 @@ import scala.concurrent.duration._
 
 class Same_Node_W_Equal_To_3 extends Simulation {
 
-  val scn = scenario("Send data to the same node with w=3 option")
-    .exec(http("PUT data")
+  def id = UUID.randomUUID().toString
+
+  val scn = scenario("Send data to the same node with w=3 option").repeat(100) {
+    exec(http("PUT data")
       .post("http://localhost:9000/put")
       .header(HttpHeaderNames.ContentType, HttpHeaderValues.ApplicationJson)
-      .body(StringBody(s"""{
-          "id"    : "${UUID.randomUUID().toString}",
+      .body(StringBody(session =>
+        s"""{
+          "id"    : "$id",
           "value" : "some value",
           "w"     : 3
         }"""))
       .asJSON
       .check(status.is(204))
-    )
-    .pause(5.seconds)
+    ).pause(100.millis)
+  }
 
-  setUp(scn.inject(atOnceUsers(1)))
+  setUp(scn.inject(atOnceUsers(10)))
 }
